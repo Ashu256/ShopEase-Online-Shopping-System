2d7f45295768578c6e6c525d3b9c3f68b6c9a7a8
Ashu256 <aashkachauhan047@gmail.com>
2026-04-27 18:46:39 +0530

    Add ProductServlet to return product JSON data
    Implement doGet method to return product data in JSON format.

--- src/servlet/ProductServlet.java
+++ src/servlet/ProductServlet.java
@@ -1,1 +1,23 @@
+package servlet;
 
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class ProductServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+
+        String json = "["
+                + "{\"id\":1,\"name\":\"iPhone 15\",\"price\":80000},"
+                + "{\"id\":2,\"name\":\"Laptop\",\"price\":60000},"
+                + "{\"id\":3,\"name\":\"Shoes\",\"price\":3000}"
+                + "]";
+
+        out.print(json);
+    }
+}

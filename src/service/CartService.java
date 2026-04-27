ce81a86ed8ffdf59e811d8f7b28dd4c3db8466b1
Ashu256 <aashkachauhan047@gmail.com>
2026-04-27 18:35:43 +0530

    Implement CartService for managing shopping cart

--- src/service/CartService.java
+++ src/service/CartService.java
@@ -1,1 +1,16 @@
+package service;
 
+import java.util.ArrayList;
+import model.Product;
+
+public class CartService {
+    private static ArrayList<Product> cart = new ArrayList<>();
+
+    public static void addToCart(Product p) {
+        cart.add(p);
+    }
+
+    public static ArrayList<Product> getCart() {
+        return cart;
+    }
+}

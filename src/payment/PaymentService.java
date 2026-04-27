25c347c0b45d52bc3f32d36e9ce92f8329ed93ae
Ashu256 <aashkachauhan047@gmail.com>
2026-04-27 18:26:35 +0530

    Update PaymentService.java

--- src/payment/PaymentService.java
+++ src/payment/PaymentService.java
@@ -1,1 +1,9 @@
+package payment;
 
+public class PaymentService {
+
+    public static boolean processPayment(double amount) {
+        System.out.println("Payment of ₹" + amount + " successful!");
+        return true;
+    }
+}

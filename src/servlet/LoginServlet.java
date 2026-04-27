8cc1613416162c8008202178f50e8d51efde4bf4
Ashu256 <aashkachauhan047@gmail.com>
2026-04-27 18:43:49 +0530

    Implement LoginServlet for user authentication

--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -1,1 +1,24 @@
+package servlet;
 
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class LoginServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        response.setContentType("text/plain");
+        PrintWriter out = response.getWriter();
+
+        if (email.equals("admin@shopease.com") && password.equals("admin123")) {
+            out.print("success");
+        } else {
+            out.print("fail");
+        }
+    }
+}

98f13aa2171ca5cc4dca2348b207f4ff94822f6d
Ashu256 <aashkachauhan047@gmail.com>
2026-04-27 18:41:51 +0530

    Update CartServlet.java

--- src/servlet/CartServlet.java
+++ src/servlet/CartServlet.java
@@ -1,1 +1,43 @@
+package servlet;
 
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class CartServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        HttpSession session = request.getSession();
+        List<String> cart = (List<String>) session.getAttribute("cart");
+
+        if (cart == null) {
+            cart = new ArrayList<>();
+        }
+
+        String item = request.getParameter("item");
+        cart.add(item);
+
+        session.setAttribute("cart", cart);
+
+        response.getWriter().print("added");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        HttpSession session = request.getSession();
+        List<String> cart = (List<String>) session.getAttribute("cart");
+
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+
+        if (cart == null) {
+            out.print("[]");
+        } else {
+            out.print(cart.toString());
+        }
+    }
+}

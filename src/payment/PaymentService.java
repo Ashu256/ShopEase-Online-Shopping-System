43df04125d28eb0efc49b02c564bca057fc9be47
Ashu256 <aashkachauhan047@gmail.com>
2026-05-07 20:07:26 +0530

    Delete src/payment/PaymentService.java

--- src/payment/PaymentService.java
+++
@@ -1,9 +0,0 @@
-package payment;
-
-public class PaymentService {
-
-    public static boolean processPayment(double amount) {
-        System.out.println("Payment of ₹" + amount + " successful!");
-        return true;
-    }
-}
